c2c150c02c11efedb11a7ce167bcf8dd1cc919e5
Sheeraz Ahmed <48241465+sheerazchand@users.noreply.github.com>
2022-01-13 11:08:31 -0600

    Create hide-default-fields-from-edit-page.php

---
+++ hide-default-fields-from-edit-page.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* Here is how you can hide default fields
+* such as First Name, Last Name and Display Name
+* From Edit page of PMPro
+* Place this code in function.php
+*/
+
+function mypmpro_member_profile_edit_user_object_fields($fields) {
+	unset($fields['first_name']);
+	unset($fields['last_name']);
+	unset($fields['display_name']);
+	return $fields;
+}
+
+?>
